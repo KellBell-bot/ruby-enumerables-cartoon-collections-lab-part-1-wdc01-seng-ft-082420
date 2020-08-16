733c5b09c1e29d2cd2181031072bd8b82600b35f
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-16 16:35:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |dwarves|
-    puts dwarves
+  array.each_with_index do |dwarves, index|
+    puts " #{index +1}. dwarves"
   end
 end
 
